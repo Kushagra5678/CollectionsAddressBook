116f4a1a8ba0acdcd8b5bbd9015b600da24c4020
Kushagra5678 <kushu.7feb@gmail.com>
2020-10-04 22:04:58 +0530

    Main commit

--- addBookCollectionsStart/src/com/practice/addBookCollectionsStart/AddressBook.java
+++ addBookCollectionsStart/src/com/practice/addBookCollectionsStart/AddressBook.java
@@ -1,10 +1,11 @@
-package addressBookStart;
+package com.practice.addBookCollectionsStart;
 
 public class AddressBook {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to Address Book");
+		System.out.println("Welcome to Address Book program");
+
 	}
 
 }
